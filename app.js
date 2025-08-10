92dd5a5b7de4dc0beb61bb7e402ce60f8c3d90f5
Alejandro Cortes <aregularmail@gmail.com>
2025-08-09 20:28:07 -0600

    crea la función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById("amigo").value;
+    if (nombreAmigo === "") {
+        alert("Por favor, ingresa un nombre.");
+        return;
+    } else {
+        listaAmigos.push(nombreAmigo);
+    }
+    document.getElementById("amigo").value = "";
+    console.log(listaAmigos);
+    
+}
